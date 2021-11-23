3577f810121dae61126aad325a4a2f1efb1e7515
Jonathan Radford <jonathanradford@live.co.uk>
2021-11-23 17:49:33 +0000

    added @media screen for different sizes

---
+++ dist/translate.dev.js
@@ -0,0 +1,73 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getWord = exports.getLetter = void 0;
+
+//find out which letter has been pressed and send that informatiom to info
+var getLetter = function getLetter(message) {
+  if (message === "a") {
+    return ".-";
+  } else if (message === "b") {
+    return "-...";
+  } else if (message === "c") {
+    return "-.-.";
+  } else if (message === "d") {
+    return "-..";
+  } else if (message === "e") {
+    return ".";
+  } else if (message === "f") {
+    return "..-.";
+  } else if (message === "g") {
+    return "--.";
+  } else if (message === "h") {
+    return "....";
+  } else if (message === "i") {
+    return "..";
+  } else if (message === "j") {
+    return ".---";
+  } else if (message === "k") {
+    return "-.-";
+  } else if (message === "l") {
+    return ".-..";
+  } else if (message === "m") {
+    return "--";
+  } else if (message === "n") {
+    return "-.";
+  } else if (message === "o") {
+    return "---";
+  } else if (message === "p") {
+    return ".--.";
+  } else if (message === "q") {
+    return "--.-";
+  } else if (message === "r") {
+    return ".-.";
+  } else if (message === "s") {
+    return "...";
+  } else if (message === "t") {
+    return "-";
+  } else if (message === "u") {
+    return "..-";
+  } else if (message === "v") {
+    return "...-";
+  } else if (message === "w") {
+    return ".--";
+  } else if (message === "x") {
+    return "-..-";
+  } else if (message === "y") {
+    return "-.--";
+  } else if (message === "z") {
+    return "--..";
+  }
+};
+
+exports.getLetter = getLetter;
+
+var getWord = function getWord() {
+  var word = Array.from("hello");
+  var newWord = word.map(getLetter).join("");
+  return newWord;
+};
+
+exports.getWord = getWord;
