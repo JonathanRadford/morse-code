f7b46d83afb8de0ce54ed95562762a8a640ebfd5
Jonathan Radford <jonathanradford@live.co.uk>
2021-11-24 08:41:50 +0000

    README updated

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ I have used If Else statements to move through the Array and work out whether or
 
 We are to test all of the functions that we use, with the final product being tested as a whole.
 
-After creating a translator that translates on blue, my next aim will be to add a button that translates, plus the ability to translate in reverse, which should be simple enough.
+After creating a translator that translates on blue, my next aim will be to add a button that translates, plus the ability to translate in reverse.
+
+After adding buttons that both submit and reset the text area, my aim will be to add the ability to translate in reverse.
--- main.js
+++ main.js
@@ -2,29 +2,33 @@
 //convert the letter to morse code on click
 //return and print that information back to the html
 //take the word presented and turn it into an array
-import { getLetter} from "./translate.js";
+import { getLetter } from "./translate.js";
 
-
+//take a word, and break it down into an Array, map through the letters to convert into morsecode via the getLetter function then returns the array as a string
 let newWord = "";
-//take a word, and break it down into an Array, map through the letters to convert into morsecode via the getLetter function then returns the array as a string 
 const getWord = (e) => {
   let word = Array.from(e.target.value.toLowerCase());
   newWord = word.map(getLetter).join("");
   return newWord;
-}
+};
 
 //returns the translation to the output
 const getTranslation = () => {
   const input = newWord;
-  return output.innerHTML = input;
-}
-
+  return (output.innerHTML = input);
+};
 
 const handleReset = () => {
   location.reload();
 };
 
-const input = document.getElementById("input").addEventListener("blur", getWord)
-const submitButton = document.getElementById("btn-primary").addEventListener("click", getTranslation)
-const resetButton = document.getElementById("btn-secondary").addEventListener("click", handleReset)
-const output = document.getElementById("output")
+const input = document
+  .getElementById("input")
+  .addEventListener("blur", getWord);
+const submitButton = document
+  .getElementById("btn-primary")
+  .addEventListener("click", getTranslation);
+const resetButton = document
+  .getElementById("btn-secondary")
+  .addEventListener("click", handleReset);
+const output = document.getElementById("output");
